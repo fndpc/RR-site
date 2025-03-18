a46a84f2f840d9c0eeff9ef39599e84a506a9efb
fndpc <cherryyt681@gmail.com>
2025-03-18 16:02:37 +0300

    Added css styles for form

--- main/forms.py
+++ main/forms.py
@@ -7,3 +7,12 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'placeholder': 'username',
+                'required': 'required',
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'password',
+                'required': 'required',
+            })}
